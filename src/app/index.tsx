61783ce4276f5f23610dcb9ed430f1d35ce1c12f
Subhan Faqih <subhanfaqih123@gmail.com>
2026-09-16 20:47:29 +0700

    create new feature kali kalian

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,46 @@
-import Page1 from '@/components/page1';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import LabelInput from '@/components/labelInput';
 
 export default function Index() {
-  return <LabelInput label='Nama' placeholder='Masukkan nama' />;
+  const [nilai1, setNilai1] = useState('');
+  const [nilai2, setNilai2] = useState('');
+  const num1 = Number(nilai1) || 0;
+  const num2 = Number(nilai2) || 0;
+  const hasil = num1 * num2;
+
+  return (
+    <View style={styles.container}>
+      <LabelInput
+        label="Nilai 1"
+        placeholder="Masukkan nilai 1"
+        value={nilai1}
+        onChangeText={setNilai1}
+        keyboardType="numeric"
+      />
+
+      <LabelInput
+        label="Nilai 2"
+        placeholder="Masukkan nilai 2"
+        value={nilai2}
+        onChangeText={setNilai2}
+        keyboardType="numeric"
+      />
+
+      <Text style={styles.resultText}>Hasil Perkalian: {hasil}</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+  },
+  resultText: {
+    marginTop: 16,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
